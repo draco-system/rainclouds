a2e7644ee6e36cf79d9ba945d1b3cbe97bd3abf4
Umbra <gibson.shanny@gmail.com>
2023-07-07 16:29:41 -0600

    tried do do more shader work and it didn't work

--- BGanimations/ScreenWithMenuElements background.lua
+++ BGanimations/ScreenWithMenuElements background.lua
@@ -8,6 +8,7 @@
 return Def.ActorFrame{ Def.Sprite{
     Texture = THEME:GetPathG('','lichen'), --calling GetPathG through theme functions; uses 2 strings
     Frag = THEME:GetPathG('','shader.frag'), --shader :)
+    -- Frag = THEME:GetPathG(',','rain.frag'), for some reason it can't find the shader
     OnCommand = function(self) self:Center() :zoomto(SCREEN_WIDTH,SCREEN_HEIGHT) --setting it to the screen width + height
 end
     }
