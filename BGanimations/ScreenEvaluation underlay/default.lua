d36188e9f12e190d5f6e19aad5dd5c9a10c2a4da
Umbra <gibson.shanny@gmail.com>
2023-08-27 16:34:30 -0600

    :)

--- BGanimations/ScreenEvaluation underlay/default.lua
+++ BGanimations/ScreenEvaluation underlay/default.lua
@@ -1,6 +1,9 @@
 return Def.Quad{
     InitCommand = function(self)
         self
-        :xy(SCREEN_CENTER_X, 50)
+        :xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
+        :diffusealpha(0.2)
+        :diffuse((1/255)*121,(1/255)*139,(1/255)*168,0.3)
+        :SetSize(1000,500)
 end,
 }   
